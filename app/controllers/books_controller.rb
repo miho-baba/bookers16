32429e51118eeae4480e80dfed0f5f49b04c8fb4
EC2 Default User <ec2-user@ip-172-31-10-101.ap-northeast-1.compute.internal>
2023-08-15 14:44:38 +0000

    [Finish]課題2：アプリケーションを作成してみよう基礎編

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,14 +4,17 @@ class BooksController < ApplicationController
   @book = Book.new
   end
 
-  def create
+def create
 
-    book = Book.new(book_params)
-
-    book.save
-
-    redirect_to '/'
-  end
+ @book = Book.new(book_params)
+ if @book.save
+   flash[:notice] = "Book was successfully created."
+    redirect_to books_path(@book.id)
+ else
+      @books = Book.all
+      render :index
+ end
+end
 
   def show
    @book = Book.find(params[:id])
@@ -19,15 +22,28 @@ class BooksController < ApplicationController
   end
 
   def edit
- @book = Book.find(params[:id])
+  @book = Book.find(params[:id])
 
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to '/books'
   end
+
+  def update
+    @book = Book.find(params[:id])
+   if @book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(@book.id)
+   else
+      @books = Book.all
+      render :edit
+   end
+  end
+
   private
 
   def book_params
