5a1d869f27d88494616967d13c4971aea9f96fa5
EC2 Default User <ec2-user@ip-172-31-10-101.ap-northeast-1.compute.internal>
2023-08-16 07:51:38 +0000

    [Finish]課題２訂正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,7 @@ def create
  @book = Book.new(book_params)
  if @book.save
    flash[:notice] = "Book was successfully created."
-    redirect_to books_path(@book.id)
+    redirect_to book_path(@book.id)
  else
       @books = Book.all
       render :index
